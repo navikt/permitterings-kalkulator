a2a6cac2ddb718a1088ecc70a107318960d2af7d
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-04-01 14:10:12 +0200

    la til import paa override styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Redirect from './Redirect';
 import Permittering from './komponenter/Permittering';
+import './assets/styling/dekorator-override.less';
 
 const App = () => {
     return (
