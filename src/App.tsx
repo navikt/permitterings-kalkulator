6029b46ca23cf0daf0b07938ea56d00d2acf0006
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-04-01 12:52:45 +0200

    oppdatert workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Redirect from "./Redirect";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Redirect from './Redirect';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="arbeidsgiver-permittering">
-        <Switch>
-          <Redirect>
-            <Route
-              path={"/arbeidsgiver-permittering"}
-              component={ComponentTempHolder}
-              exact={true}
-            />
-          </Redirect>
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="arbeidsgiver-permittering">
+                <Switch>
+                    <Redirect>
+                        <Route path={'/arbeidsgiver-permittering'} component={ComponentTempHolder} exact={true} />
+                    </Redirect>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 };
 
 const ComponentTempHolder = () => (
-  <header className="App-header">
-    infosider til Arbeidsgiver Permittering under arbeid
-  </header>
+    <header className="App-header">infosider til Arbeidsgiver Permittering under arbeid</header>
 );
 
 export default App;
