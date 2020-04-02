37f27b6d87593fb9ffd6ea83bc9e56b129255503
Hanna Sofia Högberg <hanna.sofia.hogberg@nav.no>
2020-04-02 16:43:08 +0200

    fixed

--- src/komponenter/info-ark/infoark-ipermitteringsperioden/tekster/AvbrytePermitteringen.tsx
+++ src/komponenter/info-ark/infoark-ipermitteringsperioden/tekster/AvbrytePermitteringen.tsx
@@ -5,7 +5,7 @@ const AvbrytePermitteringen = () => {
         <>
             Varslet om permittering kan trekkes tilbake når som helst før iverksettelsen. Er permittering iverksatt kan
             permitteringen avbrytes når som helst uavhengig av den varighet som ble varslet. Den permitterte må
-            informere NAV at permitteringen er avbrutt. (lenke til skriv til oss)
+            informere NAV at permitteringen er avbrutt.
         </>
     );
 };
--- src/komponenter/info-ark/infoark-permittere-ansatte/lister/ItilleggBerVi.tsx
+++ src/komponenter/info-ark/infoark-permittere-ansatte/lister/ItilleggBerVi.tsx
@@ -8,11 +8,11 @@ const ItilleggBerVi = () => {
     const cls = BEMHelper(permitteringClassName);
     return (
         <div className={cls.element('blockmodifier')}>
-            <Normaltekst>I tillegg ber vi deg som arbeidsgiver oppgi følgende i varselet</Normaltekst>
+            <Normaltekst>I tillegg ber vi deg som arbeidsgiver oppgi følgende i varselet:</Normaltekst>
             <ul>
                 <li>Om det er enighet mellom partene på arbeidsplassen om permitteringen (JA/NEI)</li>
                 <li>Den permitteres stillingsstørrelse</li>
-                <li>Når den permitterte ble ansatt i bedriften.</li>
+                <li>Når den permitterte ble ansatt i bedriften</li>
             </ul>
         </div>
     );
