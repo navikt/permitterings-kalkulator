28bc079d43717766e803e20c741b552706737647
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-04-02 20:24:00 +0200

    lagt til video i info seksjon

--- src/komponenter/info-ark/infoark-permittere-ansatte/PermittereAnsatte.tsx
+++ src/komponenter/info-ark/infoark-permittere-ansatte/PermittereAnsatte.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BEMHelper from '../../../utils/bem';
 import KnappBase from 'nav-frontend-knapper';
 import Tekstseksjon from '../../infoseksjon/Tekstseksjon';
@@ -16,11 +16,27 @@ interface Props {
 
 const InfoarkPermittereAnsatte = (props: Props) => {
     const cls = BEMHelper(props.className);
+    const desktopSize = 480;
+    const tabletSize = 380;
+    const mobilSize = 280;
+
+    const setSize = () => (window.innerWidth > 1024 ? desktopSize : window.innerWidth > 768 ? tabletSize : mobilSize);
+
+    const [videoview, setVideoview] = useState<number>(setSize);
 
     const gatilSoknad = () => {
         window.location.href =
             'https://www.nav.no/soknader/nb/bedrift/permitteringer-oppsigelser-og-konkurs/masseoppsigelser';
     };
+
+    useEffect(() => {
+        const handleresize = () => {
+            return setVideoview(setSize());
+        };
+
+        window.addEventListener('resize', handleresize);
+        return () => window.removeEventListener('resize', handleresize);
+    }, []);
 
     return (
         <div className={cls.element('avsnitt')}>
@@ -38,6 +54,17 @@ const InfoarkPermittereAnsatte = (props: Props) => {
             <DersomVarselInneholder />
             <GiAnsatteBeskjed />
             <HuskArapportere />
+            <div className={cls.element('video-frame')}>
+                <iframe
+                    src="https://player.vimeo.com/video/398208025"
+                    width={videoview}
+                    height="360"
+                    frameBorder="0"
+                    allow="autoplay; fullscreen"
+                    allowFullScreen
+                    title="Permitteringsvideo for arbeidsgivere"
+                />
+            </div>
         </div>
     );
 };
