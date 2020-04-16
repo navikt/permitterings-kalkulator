eabe81f208581671ed7a5ff96b12673c06381816
Hanna Sofia Högberg <hanna.sofia.hogberg@nav.no>
2020-04-16 08:18:31 +0200

    leg til 6g

--- src/komponenter/info-ark/infoark-vanlige-sporsmaal/tekster/JegHarAlleredeMattePermittere.tsx
+++ src/komponenter/info-ark/infoark-vanlige-sporsmaal/tekster/JegHarAlleredeMattePermittere.tsx
@@ -4,12 +4,7 @@ import Lenke from 'nav-frontend-lenker';
 const JegHarAlleredeMattePermittere = () => {
     return (
         <>
-            Hvis du har utbetalt lønn utover de 2 dagene med arbeidsgiverperiode
-            før 20. mars, etter tidligere regler, vil ikke disse refunderes.
-            Lønn du har utbetalt utover de 2 dagene etter 20.mars, kan du få
-            refundert av NAV. Du får ikke refusjon for permitteringer som blir
-            iverksatt 20. april eller senere. NAV jobber med løsning for å søke
-            om refusjon. Les mer om{' '}
+            Hvis du har utbetalt lønn utover de 2 dagene med arbeidsgiverperiode før 20. mars, etter tidligere regler, vil ikke disse refunderes. Lønn, begrenset til seks ganger grunnbeløpet, du har utbetalt utover de 2 dagene etter 20.mars, kan du få refundert av NAV. Du får ikke refusjon for permitteringer som blir iverksatt 20. april eller senere. NAV jobber med løsning for å søke om refusjon. Les mer om {' '}
             <Lenke href="https://lovdata.no/dokument/SF/forskrift/2020-04-08-757">
                 refusjon av lønn.
             </Lenke>
