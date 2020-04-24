806033c48799e061c3f92466d1c7d8d232e75fea
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-04-24 19:19:58 +0200

    oppdatert tekst og lagt til illustrasjon paa avsnitt

--- src/komponenter/info-ark/infoark-utbetale-lonn/NarSkalJegUtbetaleLonn.tsx
+++ src/komponenter/info-ark/infoark-utbetale-lonn/NarSkalJegUtbetaleLonn.tsx
@@ -4,6 +4,7 @@ import Tekstseksjon from '../../infoseksjon/Tekstseksjon';
 import HvorLengeSkalJegBetaleLonn from './tekster/HvorLengeSkalJegBetaleLonn';
 import DuMaIkkeForskuttereLonn from './tekster/DuMaIkkeForskuttereLonn';
 import TilbakebetalingAvUtbetaltLonn from './tekster/TilbakebetalingAvUtbetaltLonn';
+import UtbetaleLonnIllustrasjon from './Hvor-lenge-skal-jeg-betale/UtbetaleLonnIllustrasjon';
 
 interface Props {
     className: string;
@@ -12,8 +13,11 @@ interface Props {
 const NarSkalJegUtbetaleLonn = (props: Props) => {
     const cls = BEMHelper(props.className);
     return (
-        <div className={cls.element('avsnitt')}>
+        <div className={cls.element('avsnitt', 'topmodifier')}>
             <Tekstseksjon tittel="Hvor lenge skal jeg betale lønn?">
+                <UtbetaleLonnIllustrasjon />
+            </Tekstseksjon>
+            <Tekstseksjon tittel="Permitteringen starter">
                 <HvorLengeSkalJegBetaleLonn />
             </Tekstseksjon>
             <Tekstseksjon tittel="Du må ikke forskuttere lønn for permitteringer som starter fra 20.april">
