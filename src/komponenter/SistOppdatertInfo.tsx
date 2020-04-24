734a14aa194b7b2a5cc29dc1acd5254182b9f99d
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-04-24 19:21:01 +0200

    oppdatert dato

--- src/komponenter/SistOppdatertInfo.tsx
+++ src/komponenter/SistOppdatertInfo.tsx
@@ -13,7 +13,7 @@ const SistOppdatertInfo = (props: Props) => {
             <Normaltekst className={cls.element('sist-oppdatert')}>
                 <i>
                     Denne siden blir løpende oppdatert, sist oppdatert
-                    23.04.2020 12:00
+                    24.04.2020 19:30
                 </i>
             </Normaltekst>
         </>
