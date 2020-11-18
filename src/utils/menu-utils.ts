72874a75ee3170390bdb72bee743479d79c1aa39
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-11-18 14:16:50 +0100

    refactor på menyscroll event

--- src/utils/menu-utils.ts
+++ src/utils/menu-utils.ts
@@ -2,33 +2,60 @@ const CONTAINER_WIDTH = 560;
 const STICKYHEADER_HEIGHT = 100;
 const HEADERDIFF = 46;
 
-export const isDesktop = () => window.innerWidth > 1024;
+export const windowWidthIsDesktopSize = () => window.innerWidth > 1024;
 
-const compare = (): boolean => {
-    return window.pageYOffset < getContainerHeight() - STICKYHEADER_HEIGHT;
+const getMainContainer = (): HTMLDivElement | null =>
+    document.getElementById('hvordanPermittere') as HTMLDivElement;
+
+const scrollheightIsLowerThanContainerOffsetTop = (
+    containerOffsetTop: number
+) => {
+    return window.pageYOffset < containerOffsetTop - STICKYHEADER_HEIGHT;
 };
 
-export const setScroll = () => {
-    return compare()
-        ? getContainerHeight() - window.pageYOffset
-        : STICKYHEADER_HEIGHT;
+export const adjustMenuHeight = () => {
+    const elem = getMainContainer();
+    if (elem && scrollheightIsLowerThanContainerOffsetTop(elem.offsetTop)) {
+        return getContainerHeight() - window.pageYOffset;
+    }
+    return STICKYHEADER_HEIGHT;
 };
 
-export const calcWithPosition = () => {
-    if (!isDesktop()) {
-        return window.innerWidth > CONTAINER_WIDTH
-            ? (window.innerWidth - CONTAINER_WIDTH) / 2 + 20
-            : 16;
+export const calcMenuWidthPosition = () => {
+    if (!windowWidthIsDesktopSize()) {
+        calcMobileMenuWidthPosition();
+    }
+    return 0; //desktop is 0.
+};
+
+const calcMobileMenuWidthPosition = () => {
+    const windowWidthLessThanContainerWidth =
+        window.innerWidth > CONTAINER_WIDTH;
+
+    if (windowWidthLessThanContainerWidth) {
+        return calcRightsideOffContainerWidth();
+    }
+    return 16; // 1rem indent
+};
+
+const calcRightsideOffContainerWidth = (): number =>
+    (window.innerWidth - CONTAINER_WIDTH) / 2 + 20;
+
+export const getContainerHeight = (): number => {
+    if (!windowWidthIsDesktopSize()) {
+        return getMobileContainerHeight();
+    }
+    return getDesktopContainerOffsetTopDiff();
+};
+
+const getMobileContainerHeight = (): number => {
+    const container = getMainContainer();
+    if (container) {
+        return container.offsetTop;
     }
     return 0;
 };
 
-export const getContainerHeight = (): number => {
-    const htmlcontainer = document.getElementById(
-        'hvordanPermittere'
-    ) as HTMLDivElement;
-    if (!isDesktop() && htmlcontainer) {
-        return htmlcontainer.offsetTop;
-    }
+const getDesktopContainerOffsetTopDiff = (): number => {
     return HEADERDIFF;
 };
