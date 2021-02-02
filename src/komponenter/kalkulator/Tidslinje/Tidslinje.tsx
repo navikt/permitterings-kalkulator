c09f1c16b36f472deca3c903833bb9058d502ea4
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-02-02 12:58:06 +0100

    dra over tidslinjen for a se datoer

--- src/komponenter/kalkulator/Tidslinje/Tidslinje.tsx
+++ src/komponenter/kalkulator/Tidslinje/Tidslinje.tsx
@@ -5,13 +5,26 @@ import { AllePermitteringerOgFraværesPerioder } from '../kalkulator';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { skrivOmDato } from '../../Datovelger/datofunksjoner';
 
+import Draggable from 'react-draggable';
+
 interface Props {
     allePermitteringerOgFraværesPerioder: AllePermitteringerOgFraværesPerioder;
+}
+
+const regnUtHorisontalAvstandMellomToElement = (id1: string, id2: string) => {
+    const element1 = document.getElementById(id1);
+    const element2 = document.getElementById(id2);
+    const posisjonBeskrivelse1 = element1?.getBoundingClientRect();
+    const posisjonBeskrivelse2 = element2?.getBoundingClientRect();
+    const avstand = posisjonBeskrivelse1?.left!! - posisjonBeskrivelse2?.left!!
+    return Math.abs(avstand)
 }
 
 const Tidslinje:FunctionComponent<Props> = props => {
     const [tidslinjeobjekter, setTidslinjeobjekter] = useState(konstruerTidslinje(props.allePermitteringerOgFraværesPerioder))
     const breddePerObjekt = (100/tidslinjeobjekter.length).toString()+'%';
+
+    const [datoSkalVises, setDatoSkalVises] = useState('')
 
     useEffect(() => {
         setTidslinjeobjekter(konstruerTidslinje(props.allePermitteringerOgFraværesPerioder))
@@ -28,24 +41,46 @@ const Tidslinje:FunctionComponent<Props> = props => {
     }
 
     const tidslinjeHTMLObjekt = tidslinjeobjekter
-        .map ( objekt => {
+        .map ( (objekt, indeks) => {
             const style: React.CSSProperties = {
                 width: breddePerObjekt,
                 backgroundColor: finnFarge(objekt),
             }
             return (
-                <div style={style} className={'kalkulator__tidslinjeobjekt '+ finnFarge(objekt)}/>
+                <div id = {'kalkulator-tidslinjeobjekt-'+indeks}style={style} className={'kalkulator__tidslinjeobjekt '+ finnFarge(objekt)}/>
             );
-
         })
 
+    const OnTidslinjeDrag = () => {
+        let indeksElementDato = 0;
+        let minimumAvstand = 1000
+        tidslinjeHTMLObjekt.forEach((objekt,indeks) => {
+            const avstand = regnUtHorisontalAvstandMellomToElement('draggable-periode','kalkulator-tidslinjeobjekt-'+indeks)
+            if (avstand < minimumAvstand) {
+                minimumAvstand = avstand
+                indeksElementDato = indeks
+            }
+        }
+        )
+        const dato = skrivOmDato(tidslinjeobjekter[indeksElementDato].dato)
+        setDatoSkalVises(dato)
+    }
+
     return (
-        <div className={'kalkulator__tidslinje-container start'}>
-            {tidslinjeobjekter.length>0 && <div className={'kalkulator__tidslinje-datoer'}>
-                <Normaltekst>{skrivOmDato(tidslinjeobjekter[0].dato)}</Normaltekst>
-                <Normaltekst>{skrivOmDato(tidslinjeobjekter[tidslinjeobjekter.length-1].dato)}</Normaltekst>
-            </div>}
-            <div className={'kalkulator__tidslinje'}>
+        <div className={'kalkulator__tidslinje-container start'} id={'kalkulator__tidslinje-container start'}  >
+            { tidslinjeobjekter.length >0 && <>
+                    <Draggable axis={'x'} bounds={"parent"} onDrag={() => OnTidslinjeDrag()}>
+                        <div id={'draggable-periode'} className={ 'kalkulator__draggable-periode'}>
+                            {datoSkalVises}
+                        </div>
+                    </Draggable>
+                    <div className={ 'kalkulator__tidslinje-datoer'}>
+                        <Normaltekst>{skrivOmDato(tidslinjeobjekter[0].dato)}</Normaltekst>
+                        <Normaltekst>{skrivOmDato(tidslinjeobjekter[tidslinjeobjekter.length-1].dato)}</Normaltekst>
+                    </div>
+            </>}
+
+            <div className={'kalkulator__tidslinje'} id={'kalkulator__tidslinje'}>
             {tidslinjeHTMLObjekt}
             </div>
         </div>
