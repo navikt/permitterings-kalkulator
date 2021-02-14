91fe889a19cc779ae9d3eb58ae0999d932761ea1
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-02-14 16:55:25 +0100

    delt server opp i flere filer, for mer lesbarhet

--- server.js
+++ server.js
@@ -9,33 +9,30 @@ require('dotenv').config({
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
-const fs = require('fs-extra');
-const request = require('request');
-const jsdom = require('jsdom');
+
 const NodeCache = require('node-cache');
 const sanityClient = require('@sanity/client');
+const decoratorUtils = require('./server/decorator-utils');
+const template = require('./server/template');
 
 // env
 const server = express();
 server.use(helmet());
 
 const BASE_URL = '/arbeidsgiver-permittering';
-const { JSDOM } = jsdom;
-const prop = 'innerHTML';
+
 const client = sanityClient({
-    projectId: process.env.SANITY_PROJECT_ID.trim(),
-    dataset: process.env.SANITY_DATASET.trim(),
-    token: process.env.SANITY_TOKEN.trim(),
+    projectId: process.env.SANITY_PROJECT_ID?.trim(),
+    dataset: process.env.SANITY_DATASET?.trim(),
+    token: process.env.SANITY_TOKEN?.trim(),
     useCdn: false,
 });
 
 // Cache init
-const mainCacheKey = 'permittering-withMenu';
-const backupCacheKey = 'permittering-withMenuBackup';
+
 const mainCacheInnholdKey = 'permittering-innhold';
 const backupCacheInnholdKey = 'permittering-innholdBackup';
-const mainCacheMeny = new NodeCache({ stdTTL: 900, checkperiod: 90 });
-const backupCacheMeny = new NodeCache({ stdTTL: 0, checkperiod: 0 });
+
 const mainCacheInnhold = new NodeCache({
     stdTTL: parseInt(process.env.SANITY_CACHE_TTL),
     checkperiod: parseInt(process.env.SANITY_CACHE_CHECK),
@@ -50,40 +47,18 @@ server.get('/arbeidsgiver-permittering/internal/isReady', (req, res) =>
     res.sendStatus(200)
 );
 
-const sanityQueryKeys = () => [
-    'sist-oppdatert',
-    'hvordan-permittere-ansatte',
-    'nar-skal-jeg-utbetale-lonn',
-    'i-permitteringsperioden',
-    'vanlige-sporsmal',
-];
-
-const htmlinsert = () => [
-    { inject: 'styles', from: 'styles' },
-    { inject: 'scripts', from: 'scripts' },
-    { inject: 'headerWithmenu', from: 'header-withmenu' },
-    { inject: 'footerWithmenu', from: 'footer-withmenu' },
-    { inject: 'megamenuResources', from: 'megamenu-resources' },
-];
-
 const sendDataObj = (json) => ({
     data: json,
     env: [process.env.SANITY_PROJECT_ID, process.env.SANITY_DATASET],
 });
 
-const uri =
-    'https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&level=Level4&language=nb';
-const breadcrumbs =
-    '&breadcrumbs=[{"url":"https://arbeidsgiver.nav.no/arbeidsgiver-permittering","title":"Veiviser for permittering"}]';
-
-const url = () => process.env.DECORATOR_EXTERNAL_URL || uri.concat(breadcrumbs);
-
 const sanityQuery = () =>
-    sanityQueryKeys()
+    template
+        .sanityQueryKeys()
         .map((queryfragment, index) => {
             return index === 0
                 ? `*[_type == '${queryfragment}' ||`
-                : index === sanityQueryKeys().length - 1
+                : index === template.sanityQueryKeys().length - 1
                 ? `_type == '${queryfragment}'] | order(_type, priority)`
                 : `_type == '${queryfragment}' ||`;
         })
@@ -145,52 +120,8 @@ server.get(`${BASE_URL}/innhold/`, (req, res) => {
     cacheInnhold ? res.send(sendDataObj(cacheInnhold)) : fetchInnhold(res);
 });
 
-const injectMenuIntoHtml = (menu) => {
-    fs.readFile(__dirname + '/build/index.html', 'utf8', function (err, html) {
-        if (!err) {
-            const { document } = new JSDOM(html).window;
-            htmlinsert().forEach((element) => {
-                document.getElementById(element.inject)[
-                    prop
-                ] = menu.getElementById(element.from)[prop];
-            });
-            const output = document.documentElement.innerHTML;
-            mainCacheMeny.set(mainCacheKey, output, 10000);
-            backupCacheMeny.set(backupCacheKey, output, 0);
-            serveAppWithMenu(output);
-        } else {
-            checkBackupCache();
-        }
-    });
-};
-
-const getMenu = () => {
-    request({ method: 'GET', uri: url() }, (error, response, body) => {
-        if (!error && response.statusCode >= 200 && response.statusCode < 400) {
-            const { document } = new JSDOM(body).window;
-            injectMenuIntoHtml(document);
-        } else {
-            console.log('tried to fetch menu fragments from ', `${url()}`);
-            console.log('respons failed, with response ', response);
-            console.log('error: ', error);
-            checkBackupCache();
-        }
-    });
-};
-
 const serveAppWithMenu = (app) => {
-    const staticPaths = [
-        'asset-manifest.json',
-        'manifest.json',
-        'favicon.ico',
-        'precache-manifest.*',
-        'service-worker.js',
-        'permittering.nav.illustrasjon.png',
-        'static',
-        'index.css',
-    ];
-
-    staticPaths.map((path) => serverUse(path));
+    template.staticPaths.map((path) => serverUse(path));
     server.get([`${BASE_URL}/`, `${BASE_URL}/*`], (req, res) => {
         res.send(app);
     });
@@ -212,29 +143,7 @@ const serveAppWithOutMenu = () => {
     setServerPort();
 };
 
-const getMenuAndServeApp = () => {
-    mainCacheMeny.get(mainCacheKey, (err, response) => {
-        if (!err && response !== undefined) {
-            serveAppWithMenu(response);
-        } else {
-            getMenu();
-        }
-    });
-};
+decoratorUtils.getMenuAndServeApp();
 
-const checkBackupCache = () => {
-    backupCacheMeny.get(backupCacheKey, (err, response) => {
-        if (!err && response !== undefined) {
-            mainCacheMeny.set(mainCacheKey, response, 10000);
-            serveAppWithMenu(response);
-        } else {
-            console.log('failed to fetch menu');
-            console.log(
-                'cache store empty, serving app with out menu fragments'
-            );
-            serveAppWithOutMenu();
-        }
-    });
-};
-
-getMenuAndServeApp();
+module.exports.serveAppWithOutMenu = serveAppWithOutMenu;
+module.exports.serveAppWithMenu = serveAppWithMenu;
---
+++ server/decorator-utils.js
@@ -0,0 +1,77 @@
+const request = require('request');
+const jsdom = require('jsdom');
+const server = require('../server');
+const template = require('./template');
+const fs = require('fs-extra');
+const NodeCache = require('node-cache');
+
+const { JSDOM } = jsdom;
+const prop = 'innerHTML';
+
+// Cache init
+const mainCacheKey = 'permittering-withMenu';
+const backupCacheKey = 'permittering-withMenuBackup';
+const mainCacheMeny = new NodeCache({ stdTTL: 900, checkperiod: 90 });
+const backupCacheMeny = new NodeCache({ stdTTL: 0, checkperiod: 0 });
+
+const injectMenuIntoHtml = (menu) => {
+    fs.readFile(__dirname + '/build/index.html', 'utf8', function (err, html) {
+        if (!err) {
+            const { document } = new JSDOM(html).window;
+            template.decoratorTemplatekeys().forEach((element) => {
+                document.getElementById(element.inject)[
+                    prop
+                ] = menu.getElementById(element.from)[prop];
+            });
+            const output = document.documentElement.innerHTML;
+            mainCacheMeny.set(mainCacheKey, output, 10000);
+            backupCacheMeny.set(backupCacheKey, output, 0);
+            server.serveAppWithMenu(output);
+        } else {
+            checkBackupCache();
+        }
+    });
+};
+
+const getMenu = () => {
+    request({ method: 'GET', uri: template.url() }, (error, response, body) => {
+        if (!error && response.statusCode >= 200 && response.statusCode < 400) {
+            const { document } = new JSDOM(body).window;
+            injectMenuIntoHtml(document);
+        } else {
+            console.log('tried to fetch menu fragments from ', `${url()}`);
+            console.log('respons failed, with response ', response);
+            console.log('error: ', error);
+            checkBackupCache();
+        }
+    });
+};
+
+const getMenuAndServeApp = () => {
+    mainCacheMeny.get(mainCacheKey, (err, response) => {
+        if (!err && response !== undefined) {
+            server.serveAppWithMenu(response);
+        } else {
+            getMenu();
+        }
+    });
+};
+
+const checkBackupCache = () => {
+    backupCacheMeny.get(backupCacheKey, (err, response) => {
+        if (!err && response !== undefined) {
+            mainCacheMeny.set(mainCacheKey, response, 10000);
+            server.serveAppWithMenu(response);
+        } else {
+            console.log('failed to fetch menu');
+            console.log(
+                'cache store empty, serving app with out menu fragments'
+            );
+            server.serveAppWithOutMenu();
+        }
+    });
+};
+
+module.exports.getMenu = getMenu;
+module.exports.injectMenuIntoHtml = injectMenuIntoHtml;
+module.exports.getMenuAndServeApp = getMenuAndServeApp;
