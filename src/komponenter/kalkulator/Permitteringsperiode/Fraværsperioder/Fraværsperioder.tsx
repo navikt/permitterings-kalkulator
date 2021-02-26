95e3fd07619983c8ecdac47b2f89a1e832bc4dd9
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-02-26 08:55:23 +0100

    Fjern ubrukt import

--- src/komponenter/kalkulator/Permitteringsperiode/Fraværsperioder/Fraværsperioder.tsx
+++ src/komponenter/kalkulator/Permitteringsperiode/Fraværsperioder/Fraværsperioder.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import './Fraværsperioder.less';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import {
