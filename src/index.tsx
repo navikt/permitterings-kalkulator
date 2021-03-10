2771825ae6b667edb42b4fb5c8b1abe60c4e45c0
Mattias Lundmark <lundmark.mattias@gmail.com>
2021-03-10 10:56:34 +0100

    Lagt til sentry for å få oversikt over exceptions

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React, { Suspense } from 'react';
+import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './index.less';
 import App from './App';
 import NavFrontendSpinner from 'nav-frontend-spinner';
+import { isProduction } from './utils/fetch-utils';
+
+Sentry.init({
+    dsn: 'https://755254da014443dfb72b717063ba7652@sentry.gc.nav.no/72',
+    environment: isProduction() ? 'production' : 'local',
+    enabled: isProduction(),
+});
 
 ReactDOM.render(
     <Suspense fallback={<NavFrontendSpinner />}>
