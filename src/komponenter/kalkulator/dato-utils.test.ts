ff84a930a729989d1977d99440f7bc22cdf22db6
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-03-29 12:20:36 +0200

    Flytt testene ut til sine egne filer

---
+++ src/komponenter/kalkulator/dato-utils.test.ts
@@ -0,0 +1,186 @@
+import {
+    finnInitialgrenserForTidslinjedatoer,
+    konstruerTidslinje,
+} from './tidslinje-utils';
+import dayjs from 'dayjs';
+import {
+    antallDagerGått,
+    finnSisteDato,
+    finnTidligsteDato,
+    finnUtOmDefinnesOverlappendePerioder,
+    getAntallOverlappendeDager,
+    kuttAvDatoIntervallInnefor18mnd,
+} from './dato-utils';
+import { DatoIntervall } from './typer';
+import { configureDayJS } from '../../dayjs-config';
+
+configureDayJS();
+
+describe('Tester for utregninger.ts', () => {
+    test('antall dager mellom to datoer teller riktig for et tilfeldig utvalg av 1000 datoer i tidslinja', () => {
+        const tidslinje = konstruerTidslinje(
+            { permitteringer: [], andreFraværsperioder: [] },
+            dayjs().startOf('date'),
+            finnInitialgrenserForTidslinjedatoer(dayjs().startOf('date'))
+                .datoTil!
+        );
+        for (let i = 0; i < 1000; i++) {
+            const tilfeldigIndeks = Math.floor(
+                Math.random() * tidslinje.length
+            );
+            const utregnetAntallDagerGått = antallDagerGått(
+                tidslinje[0].dato,
+                tidslinje[tilfeldigIndeks].dato
+            );
+            const riktigAntallDagerGått = tilfeldigIndeks + 1;
+            expect(utregnetAntallDagerGått).toBe(riktigAntallDagerGått);
+        }
+    });
+
+    test('Antall dager mellom to datoer', () => {
+        const enDagIFebruar = dayjs('2021-02-20');
+        const nesteDagIFebruar = dayjs('2021-02-21');
+        const enDagIMars = dayjs('2021-03-23');
+        const enDagIMarsEtÅrSenere = dayjs('2022-03-23');
+
+        expect(antallDagerGått(enDagIFebruar, enDagIFebruar)).toBe(1);
+        expect(antallDagerGått(enDagIFebruar, nesteDagIFebruar)).toBe(2);
+        expect(antallDagerGått(enDagIFebruar, enDagIMars)).toBe(32);
+        expect(antallDagerGått(enDagIMars, enDagIMarsEtÅrSenere)).toBe(366);
+    });
+
+    test('Tester om to datointervaller er overlappende. Samme slutt og startdato skal regnes som overlappende.', () => {
+        const startIntervall1 = dayjs('2021-03-01');
+        const sluttIntervall1 = dayjs('2021-04-15');
+
+        const startIntervall2 = dayjs('2021-04-15');
+        const sluttIntervall2 = dayjs('2021-05-15');
+
+        const periode1: DatoIntervall = {
+            datoFra: startIntervall1,
+            datoTil: sluttIntervall1,
+        };
+
+        const periode2: DatoIntervall = {
+            datoFra: startIntervall2,
+            datoTil: sluttIntervall2,
+        };
+
+        expect(
+            finnUtOmDefinnesOverlappendePerioder(Array.of(periode1, periode2))
+        ).toBe(true);
+    });
+
+    test('Finner den tidligste datoen fra en liste av flere permitteringsperioder', () => {
+        const startPermitteringsPeriode1 = dayjs('2021-02-14');
+        const sluttPermitteringsPeriode1 = dayjs('2021-03-02');
+        const startPermitteringsPeriode2 = dayjs('2021-01-10');
+        const sluttPermitteringsPeriode2 = dayjs('2021-01-25');
+        const startPermitteringsPeriode3 = dayjs('2021-04-14');
+        const sluttPermitteringsPeriode3 = dayjs('2021-04-28');
+        const startPermitteringsPeriode4 = dayjs('2021-05-14');
+        const sluttPermitteringsPeriode4 = dayjs('2021-06-02');
+
+        const permitteringsPeriode1: DatoIntervall = {
+            datoFra: startPermitteringsPeriode1,
+            datoTil: sluttPermitteringsPeriode1,
+        };
+        const permitteringsPeriode2: DatoIntervall = {
+            datoFra: startPermitteringsPeriode2,
+            datoTil: sluttPermitteringsPeriode2,
+        };
+        const permitteringsPeriode3: DatoIntervall = {
+            datoFra: startPermitteringsPeriode3,
+            datoTil: sluttPermitteringsPeriode3,
+        };
+        const permitteringsPeriode4: DatoIntervall = {
+            datoFra: startPermitteringsPeriode4,
+            datoTil: sluttPermitteringsPeriode4,
+        };
+
+        expect(
+            finnTidligsteDato(
+                Array.of(
+                    permitteringsPeriode1,
+                    permitteringsPeriode2,
+                    permitteringsPeriode3,
+                    permitteringsPeriode4
+                )
+            )
+        ).toEqual(dayjs('2021-01-10'));
+    });
+
+    test('Finner den siste datoen fra en liste av flere permitteringsperioder', () => {
+        const startPermitteringsPeriode1 = dayjs('2021-02-14');
+        const sluttPermitteringsPeriode1 = dayjs('2021-03-02');
+        const startPermitteringsPeriode2 = dayjs('2021-01-10');
+        const sluttPermitteringsPeriode2 = dayjs('2021-01-25');
+        const startPermitteringsPeriode3 = dayjs('2021-04-14');
+        const sluttPermitteringsPeriode3 = dayjs('2021-04-28');
+        const startPermitteringsPeriode4 = dayjs('2021-05-14');
+        const sluttPermitteringsPeriode4 = dayjs('2021-06-02');
+
+        const permitteringsPeriode1: DatoIntervall = {
+            datoFra: startPermitteringsPeriode1,
+            datoTil: sluttPermitteringsPeriode1,
+        };
+        const permitteringsPeriode2: DatoIntervall = {
+            datoFra: startPermitteringsPeriode2,
+            datoTil: sluttPermitteringsPeriode2,
+        };
+        const permitteringsPeriode3: DatoIntervall = {
+            datoFra: startPermitteringsPeriode3,
+            datoTil: sluttPermitteringsPeriode3,
+        };
+        const permitteringsPeriode4: DatoIntervall = {
+            datoFra: startPermitteringsPeriode4,
+            datoTil: sluttPermitteringsPeriode4,
+        };
+
+        expect(
+            finnSisteDato(
+                Array.of(
+                    permitteringsPeriode1,
+                    permitteringsPeriode2,
+                    permitteringsPeriode3,
+                    permitteringsPeriode4
+                )
+            )
+        ).toEqual(dayjs('2021-06-02'));
+    });
+
+    test('Kutt av datoer for en permitteringsperiode', () => {
+        const startIntervall = dayjs('2020-02-14');
+        const sluttIntervall = dayjs('2020-05-02');
+
+        const intervall: DatoIntervall = {
+            datoFra: startIntervall,
+            datoTil: sluttIntervall,
+        };
+        const startKuttDato = dayjs('2020-03-02');
+        const sluttKuttDato = dayjs('2020-04-20');
+
+        const nyttIntervall: DatoIntervall = kuttAvDatoIntervallInnefor18mnd(
+            intervall,
+            startKuttDato,
+            sluttKuttDato
+        );
+        expect(nyttIntervall.datoFra).toEqual(startKuttDato);
+        expect(nyttIntervall.datoTil).toEqual(sluttKuttDato);
+    });
+
+    test('getAntallOverlappendeDager skal telle riktig når ett intervall er løpende', () => {
+        const løpendeIntervall: DatoIntervall = {
+            datoFra: dayjs('2021-03-1'),
+            erLøpende: true,
+        };
+        const annetIntervall: DatoIntervall = {
+            datoFra: dayjs('2021-02-14'),
+            datoTil: dayjs('2021-03-14'),
+        };
+
+        expect(
+            getAntallOverlappendeDager(løpendeIntervall, annetIntervall)
+        ).toEqual(14);
+    });
+});
