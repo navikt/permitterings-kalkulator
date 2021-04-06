378ddde7676123d29e16c62d77442a44b363c5b0
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-04-06 14:55:09 +0200

    Kutt av datointervaller som går utenfor 18mndsperiode

--- src/komponenter/kalkulator/SeResultat/DetaljertUtregning/DetaljertUtregning.tsx
+++ src/komponenter/kalkulator/SeResultat/DetaljertUtregning/DetaljertUtregning.tsx
@@ -2,7 +2,10 @@ import React, { FunctionComponent } from 'react';
 import UtregningAvEnkeltPeriode from './UtregningAvEnkeltPeriode/UtregningAvEnkeltPeriode';
 import { DatoIntervall, DatoMedKategori } from '../../typer';
 import { getPermitteringsoversikt } from '../../utils/beregningerForAGP2';
-import { formaterDatoIntervall } from '../../utils/dato-utils';
+import {
+    formaterDatoIntervall,
+    getOverlappendePeriode,
+} from '../../utils/dato-utils';
 
 interface Props {
     tidslinje: DatoMedKategori[];
@@ -15,21 +18,26 @@ export const DetaljertUtregning: FunctionComponent<Props> = ({
     permitteringsperioder,
     aktuell18mndsperiode,
 }) => {
-    const permitteringsperioderInnenfor18mndsperiode = permitteringsperioder.map()
+    const permitteringsperioderInnenfor18mndsperiode: DatoIntervall[] = permitteringsperioder
+        .map((periode) => getOverlappendePeriode(periode, aktuell18mndsperiode))
+        .filter((periode) => periode !== undefined) as DatoIntervall[];
+
     return (
         <div>
             detaljert utregning for 18-månedsperioden{' '}
             {formaterDatoIntervall(aktuell18mndsperiode)}
-            {permitteringsperioder.map((periode, index) => (
-                <UtregningAvEnkeltPeriode
-                    permitteringsperiode={periode}
-                    permitteringsoversikt={getPermitteringsoversikt(
-                        tidslinje,
-                        periode
-                    )}
-                    permitteringsnr={index + 1}
-                />
-            ))}
+            {permitteringsperioderInnenfor18mndsperiode.map(
+                (periode, index) => (
+                    <UtregningAvEnkeltPeriode
+                        permitteringsperiode={periode}
+                        permitteringsoversikt={getPermitteringsoversikt(
+                            tidslinje,
+                            periode
+                        )}
+                        permitteringsnr={index + 1}
+                    />
+                )
+            )}
         </div>
     );
 };
