f39e0a269933fa08f3280c145cf83735965a0ecd
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-04-12 11:21:29 +0200

    Tillat fraværsperiodene å være ugydlige i global state

--- src/komponenter/kalkulator/Fraværsperioder/Fraværsperiode.tsx
+++ src/komponenter/kalkulator/Fraværsperioder/Fraværsperiode.tsx
@@ -1,7 +1,10 @@
 import React, { FunctionComponent, useState } from 'react';
 import { AllePermitteringerOgFraværesPerioder, DatoIntervall } from '../typer';
 import DatoIntervallInput from '../DatointervallInput/DatointervallInput';
-import { datoIntervallErGyldig, fraværInngårIPermitteringsperioder } from '../utils/dato-utils';
+import {
+    datoIntervallErGyldig,
+    fraværInngårIPermitteringsperioder,
+} from '../utils/dato-utils';
 
 interface Props {
     indeks: number;
@@ -11,44 +14,38 @@ interface Props {
     ) => void;
 }
 
-const Fraværsperiode: FunctionComponent<Props> = ({ indeks, allePermitteringerOgFraværesPerioder, setAllePermitteringerOgFraværesPerioder
+const Fraværsperiode: FunctionComponent<Props> = ({
+    indeks,
+    allePermitteringerOgFraværesPerioder,
+    setAllePermitteringerOgFraværesPerioder,
 }) => {
-    const [uvalidertDatoIntervall, setUvalidertDatoIntervall] = useState<
-        Partial<DatoIntervall>
-        >({ datoFra: undefined, datoTil: undefined });
-
     const [
         advarselFraværsperiodeUtenforPermittering,
         setAdvarselFraværsperiodeUtenforPermittering,
     ] = useState<string>('');
 
-    const oppdaterDatoIntervall = (
-        datoIntervall: Partial<DatoIntervall>
-    ) => {
-        setUvalidertDatoIntervall(datoIntervall)
-        if (datoIntervallErGyldig(datoIntervall)) {
-            if (
-                datoIntervall.datoFra &&
-                !fraværInngårIPermitteringsperioder(
-                    allePermitteringerOgFraværesPerioder.permitteringer,
-                    datoIntervall
-                )
-            ) {
-                setAdvarselFraværsperiodeUtenforPermittering(
-                    'Fravær som ikke inngår i permitteringsperioder telles ikke med i beregningen'
-                );
-            } else {
-                setAdvarselFraværsperiodeUtenforPermittering('');
-            }
-            const kopiAvFraværsperioder = [
-                ...allePermitteringerOgFraværesPerioder.andreFraværsperioder,
-            ];
-            kopiAvFraværsperioder[indeks] = datoIntervall;
-            setAllePermitteringerOgFraværesPerioder({
-                ...allePermitteringerOgFraværesPerioder,
-                andreFraværsperioder: kopiAvFraværsperioder,
-            });
+    const oppdaterDatoIntervall = (datoIntervall: Partial<DatoIntervall>) => {
+        if (
+            datoIntervall.datoFra &&
+            !fraværInngårIPermitteringsperioder(
+                allePermitteringerOgFraværesPerioder.permitteringer,
+                datoIntervall
+            )
+        ) {
+            setAdvarselFraværsperiodeUtenforPermittering(
+                'Fravær som ikke inngår i permitteringsperioder telles ikke med i beregningen'
+            );
+        } else {
+            setAdvarselFraværsperiodeUtenforPermittering('');
         }
+        const kopiAvFraværsperioder = [
+            ...allePermitteringerOgFraværesPerioder.andreFraværsperioder,
+        ];
+        kopiAvFraværsperioder[indeks] = datoIntervall;
+        setAllePermitteringerOgFraværesPerioder({
+            ...allePermitteringerOgFraværesPerioder,
+            andreFraværsperioder: kopiAvFraværsperioder,
+        });
     };
 
     const slettFraværsperiode = () => {
@@ -58,25 +55,20 @@ const Fraværsperiode: FunctionComponent<Props> = ({ indeks, allePermitteringerO
         if (kopiAvAllPermitteringsInfo.andreFraværsperioder.length > 0) {
             kopiAvAllPermitteringsInfo.andreFraværsperioder.splice(indeks, 1);
         }
-        setAllePermitteringerOgFraværesPerioder(
-            kopiAvAllPermitteringsInfo
-        );
+        setAllePermitteringerOgFraværesPerioder(kopiAvAllPermitteringsInfo);
     };
 
     return (
         <DatoIntervallInput
             advarsel={advarselFraværsperiodeUtenforPermittering}
             key={indeks}
-            datoIntervall={
-                uvalidertDatoIntervall
-            }
+            datoIntervall={allePermitteringerOgFraværesPerioder.andreFraværsperioder[indeks]}
             setDatoIntervall={(datoIntervall) =>
                 oppdaterDatoIntervall(datoIntervall)
             }
             slettPeriode={() => slettFraværsperiode()}
         />
-        )
+    );
 };
 
-export default Fraværsperiode
-
+export default Fraværsperiode;
