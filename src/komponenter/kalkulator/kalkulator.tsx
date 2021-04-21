84bf03c7dfdc5330dacc454a18c30509754d41be
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-04-21 15:41:54 +0200

    Legg til varsel om regelendring fra nho og lo

--- src/komponenter/kalkulator/kalkulator.tsx
+++ src/komponenter/kalkulator/kalkulator.tsx
@@ -2,12 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import './kalkulator.less';
 
 import Banner from '../banner/Banner';
-import { Innholdstittel } from 'nav-frontend-typografi';
+import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
 import Fraværsperioder from './Fraværsperioder/Fraværsperioder';
 import { AllePermitteringerOgFraværesPerioder, DatoMedKategori } from './typer';
 import Topp from './Topp/Topp';
 import { PermitteringContext } from '../ContextProvider';
-import { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import { Permitteringsperioder } from './Permitteringsperioder/Permitteringsperioder';
 import {
     finnInitialgrenserForTidslinjedatoer,
@@ -15,6 +15,8 @@ import {
     regnUtHvaSisteDatoPåTidslinjenSkalVære,
 } from './utils/tidslinje-utils';
 import { SeResultat } from './SeResultat/SeResultat';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import { formaterDato } from './utils/dato-utils';
 
 const Kalkulator = () => {
     const { dagensDato, innføringsdatoAGP2 } = useContext(PermitteringContext);
@@ -71,6 +73,32 @@ const Kalkulator = () => {
                 </span>
             </Banner>
             <div className={'kalkulator'}>
+                <AlertStripeAdvarsel className="kalkulator__advarsel">
+                    <Normaltekst className="kalkulator__advarsel-bold">
+                        {formaterDato(dayjs('2021-04-19'))}
+                    </Normaltekst>
+                    <Normaltekst>
+                        <span className="kalkulator__advarsel-bold">
+                            Ny partsavtale om arbeid under permitteringsperioden
+                        </span>{' '}
+                        NHO og LO har inngått ny avtale om forlenge den
+                        tidligere avbruddsperioden fra 6 til 10 uker som gjør at
+                        en ansatt kan bli tatt tilbake tilbake til noe
+                        arbeid/korte oppdrag under permitteringsperioden.{' '}
+                        <a
+                            href="https://www.nho.no/tema/arbeidsliv/artikler/nye-permitteringsregler/"
+                            className="lenke"
+                        >
+                            Se NHO-side om avtalen.
+                        </a>
+                    </Normaltekst>
+
+                    <Normaltekst className="kalkulator__advarsel-bold">
+                        <br />
+                        Vi jobber nå med å tydeliggjøre detailene fra avtalen og
+                        vil implementer dette i denne veiviseren!
+                    </Normaltekst>
+                </AlertStripeAdvarsel>
                 <Innholdstittel tag="h2">
                     Regn ut når du skal betale arbeidsgiverperiode 2
                 </Innholdstittel>
