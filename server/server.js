0e82889fccc7c011e4a22cdf0a5f25430d9c5c9a
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-04-21 14:27:03 +0200

    fjerna sanitygreier

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ require('./set-env-variables');
 require('console-stamp')(console, '[HH:MM:ss.l]');
 
 const express = require('express');
-const sanity = require('./sanity-utils');
-const template = require('./template');
 const { getHtmlWithDecorator } = require('./decorator-utils');
 const path = require('path');
 const helmet = require('helmet');
@@ -12,33 +10,6 @@ const server = express();
 const PORT = process.env.PORT || 3000;
 const buildPath = path.join(__dirname, '../build');
 const BASE_PATH = '/arbeidsgiver-permittering/kalkulator';
-
-const sendDataObj = (json) => ({
-    data: json,
-    env: [process.env.SANITY_PROJECT_ID, process.env.SANITY_DATASET],
-});
-
-const addHeadersForCertainRequests = () =>
-    server.use((req, res, next) => {
-        if (template.corsWhitelist.includes(req.headers.origin)) {
-            res.header('Access-Control-Allow-Origin', req.headers.origin);
-            res.header(
-                'Access-Control-Allow-Headers',
-                'Origin, X-Requested-With, Content-Type, Accept'
-            );
-            res.header(
-                'Access-Control-Allow-Methods',
-                'GET, HEAD, OPTIONS, POST, PUT'
-            );
-            res.setHeader(
-                'Access-Control-Allow-Headers',
-                'X-Requested-With,content-type'
-            );
-            res.setHeader('Access-Control-Allow-Credentials', true);
-        }
-
-        next();
-    });
 
 const startServer = () => {
     server.use(
@@ -86,17 +57,6 @@ const startServer = () => {
             },
         })
     );
-
-    addHeadersForCertainRequests();
-
-    server.get(`${BASE_PATH}/innhold/`, (req, res) => {
-        const cacheInnhold = sanity.mainCacheInnhold.get(
-            sanity.mainCacheInnholdKey
-        );
-        cacheInnhold
-            ? res.send(sendDataObj(cacheInnhold))
-            : sanity.fetchInnhold(res);
-    });
 
     server.use(BASE_PATH + '/', express.static(buildPath, { index: false }));
 
