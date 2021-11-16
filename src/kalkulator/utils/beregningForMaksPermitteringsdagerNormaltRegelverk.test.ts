cb7ac2bb3328702516382922f7cebf193e2d92f1
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-11-16 13:30:29 +0100

    lagt til deploy til labs

--- src/kalkulator/utils/beregningForMaksPermitteringsdagerNormaltRegelverk.test.ts
+++ src/kalkulator/utils/beregningForMaksPermitteringsdagerNormaltRegelverk.test.ts
@@ -1,5 +1,5 @@
 import { DatoIntervall } from '../typer';
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import {
     finnFørsteDatoMedPermitteringUtenFravær,
     finnInitialgrenserForTidslinjedatoer,
