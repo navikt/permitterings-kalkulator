6e3377e512669846957daf92fdab0aaa5e18f6c8
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2022-03-29 15:22:36 +0200

    fjerna sanity

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
 import React, { Suspense } from 'react';
-import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 import { configureDayJS } from './dayjs-config';
-import { isProduction } from './utils/env-utils';
 
 configureDayJS();
-
-Sentry.init({
-    dsn: 'https://755254da014443dfb72b717063ba7652@sentry.gc.nav.no/72',
-    environment: isProduction() ? 'production' : 'local',
-    enabled: isProduction(),
-});
 
 ReactDOM.render(
     <Suspense fallback={<NavFrontendSpinner />}>
