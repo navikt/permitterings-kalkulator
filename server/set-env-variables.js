09becf8331f74ddace460ab781803c51ad0cf0ee
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2022-03-29 15:20:49 +0200

    oppdatert dependencies

--- server/set-env-variables.js
+++
@@ -1,8 +0,0 @@
-const VAULT_PATH = '/var/run/secrets/nais.io/vault/environment.env';
-const path = require('path');
-require('dotenv').config({
-    path:
-        process.env.NODE_ENV === 'production'
-            ? VAULT_PATH
-            : path.join(__dirname, './../.env'),
-});
